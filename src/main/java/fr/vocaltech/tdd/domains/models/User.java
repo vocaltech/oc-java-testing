6a4b97037d46f76798e3ace3b487450158e6cb74
vocaltech <it@vocaltech.fr>
2024-06-17 14:04:35 +0200

    Refactor User as record

--- src/main/java/fr/vocaltech/tdd/domains/models/User.java
+++ src/main/java/fr/vocaltech/tdd/domains/models/User.java
@@ -1,13 +1,3 @@
 package fr.vocaltech.tdd.domains.models;
 
-public class User {
-    private String fullName;
-
-    public User(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-}
+public record User(String fullName) {}
