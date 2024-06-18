62ff1e819e0a76228e7895e8e4f9fb6534405a62
vocaltech <it@vocaltech.fr>
2024-06-18 23:59:47 +0200

    Initial commit

---
+++ src/test/java/fr/vocaltech/tdd/services/CalculatorServiceTest.java
@@ -0,0 +1,12 @@
+package fr.vocaltech.tdd.services;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorServiceTest {
+    @Test
+    void givenTwoPositiveNumbers_whenAdd_thenReturnsTheSum() {
+
+    }
+
+}
