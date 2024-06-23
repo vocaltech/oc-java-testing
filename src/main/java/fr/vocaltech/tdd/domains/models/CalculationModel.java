bd31ed1ba676d3c566980d35662888cabd0b0bd6
vocaltech <it@vocaltech.fr>
2024-06-23 23:42:22 +0200

    Add fromText() method

--- src/main/java/fr/vocaltech/tdd/domains/models/CalculationModel.java
+++ src/main/java/fr/vocaltech/tdd/domains/models/CalculationModel.java
@@ -1,3 +1,13 @@
 package fr.vocaltech.tdd.domains.models;
 
-public record CalculationModel(CalculationType calculationType, Integer leftOperand, Integer rightOperand) {}
+public record CalculationModel(CalculationType calculationType, Integer leftOperand, Integer rightOperand) {
+    public static CalculationModel fromText(String calculation) {
+        String[] parts = calculation.split(" ");
+
+        return new CalculationModel(
+                CalculationType.fromSymbol(parts[1]),
+                Integer.valueOf(parts[0]),
+                Integer.valueOf(parts[2])
+        );
+    }
+}
