c71cb6dcb2160279539e8b872861fbe9672b12ec
vocaltech <it@vocaltech.fr>
2024-06-23 23:44:14 +0200

    Remove fromText() method

--- src/main/java/fr/vocaltech/tdd/services/BatchCalculatorServiceImpl.java
+++ src/main/java/fr/vocaltech/tdd/services/BatchCalculatorServiceImpl.java
@@ -2,7 +2,6 @@ package fr.vocaltech.tdd.services;
 
 import fr.vocaltech.tdd.domains.models.CalculationModel;
 import fr.vocaltech.tdd.domains.models.CalculationSolution;
-import fr.vocaltech.tdd.domains.models.CalculationType;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -14,18 +13,8 @@ public class BatchCalculatorServiceImpl implements BatchCalculatorService {
     }
     @Override
     public List<CalculationSolution> batchCalculate(Stream<String> operations) {
-        return operations
-                .map(operation -> calculatorService.calculate(fromText(operation)))
-                .toList();
-    }
-
-    private CalculationModel fromText(String calculation) {
-        String[] parts = calculation.split(" ");
-
-        return new CalculationModel(
-                CalculationType.fromSymbol(parts[1]),
-                Integer.valueOf(parts[0]),
-                Integer.valueOf(parts[2])
-        );
+         return operations
+                 .map(operation -> calculatorService.calculate(CalculationModel.fromText(operation)))
+                 .toList();
     }
 }
